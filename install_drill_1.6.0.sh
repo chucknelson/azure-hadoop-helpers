47557881ebba88d925d9623cfbbba8ea97345297
Nelson <Charles.Nelson@carolinashealthcare.org>
2016-05-25 17:26:59 -0400

    Minor additions, still a work in progress.

--- install_drill_1.6.0.sh
+++ install_drill_1.6.0.sh
@@ -7,8 +7,10 @@ DRILL_VERSION=1.6.0
 DRILL_TAR_FILE=apache-drill-$DRILL_VERSION.tar.gz
 DRILL_DOWNLOAD_URI=http://mirrors.ocf.berkeley.edu/apache/drill/drill-$DRILL_VERSION/$DRILL_TAR_FILE
 
-#Azure storage variables
-
+#Azure dependency variables
+HADOOP_AZURE_VERSION=2.7.2
+HADOOP_AZURE_JAR_FILE=hadoop-azure-$HADOOP_AZURE_VERSION.jar
+HADOOP_AZURE_JAR_URI=http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/$HADOOP_AZURE_VERSION/$HADOOP_AZURE_JAR_FILE
 
 #Download and install/extract drill
 mkdir $DRILL_INSTALL_PATH
